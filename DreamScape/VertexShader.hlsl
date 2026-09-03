26db1757c28d8785ed2601001b96896e4bbd4178
Cirrol <filip.persson@live.se>
2026-09-03 14:49:55 +0200

    Shader pipeline fix
    Fixed an issue where the vertexshader never did an output of uv when the pixelshader used the input.

--- DreamScape/VertexShader.hlsl
+++ DreamScape/VertexShader.hlsl
@@ -18,5 +18,6 @@ VertexShaderOutput main(VertexShaderInput input)
     VertexShaderOutput output;
     output.position = float4(input.postion, 1.0f);
     output.colour = input.colour;
+    output.uv = input.uv;
     return output;
 }
